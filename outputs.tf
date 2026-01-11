e6e74563a29f57f4026db1b5933b4f04de83da46
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:46:58 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "management_group_policy_remediations" {
+  description = "All management_group_policy_remediation resources"
+  value       = azurerm_management_group_policy_remediation.management_group_policy_remediations
+}
+
